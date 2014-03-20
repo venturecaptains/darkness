41ae70d8bcf2a13972eae44bd0bb44aad3450bed
Ben Rady <benrady@gmail.com>
2014-03-19 22:52:42 -0500

    Forgot to check in tests

--- spec/javascripts/darkness_spec.js
+++ spec/javascripts/darkness_spec.js
@@ -1,6 +1,31 @@
 describe('Darkness', function() {
   it('adds sources from a list', function() {
-    addSources([{name: "one", lightlevel: 1}]);
+    addSources([{name: "one"}]);
     expect($('#light-sources option[value=one]')).toExist();
+  });
+
+  describe('when the selected source changes', function() {
+    beforeEach(function() {
+      addSources([
+        {name: "magic", spellLevel: 1},
+        {name: "nonmagic"}
+      ]);
+    });
+
+    it('shows the spell level control if the selected option is magical', function() {
+      sourceChange();
+      expect($('.magic-control')).toBeVisible();
+    });
+
+    it('hides the spell level control if the selected option is not magical', function() {
+      $('#light-sources').val('nonmagic').change();
+      sourceChange();
+      expect($('.magic-control')).toBeHidden();
+    });
+
+    it('sets the slider to the default spell level', function() {
+      sourceChange();
+      expect($('.magic-control input').val()).toEqual("1");
+    });
   });
 });
