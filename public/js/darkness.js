c2bfdf039cdaf975051967af4640570ffca4d711
Ben Rady <benrady@gmail.com>
2014-03-20 21:23:24 -0500

    Added effects to sources.json

--- public/js/darkness.js
+++ public/js/darkness.js
@@ -1,7 +1,22 @@
+// Levels
+// bright
+// normal
+// low-light
+// dark
+// magical dark
+
+// Effects:
+// daylight -> bright
+// light -> normal
+// increase +1
+// decrease -1
+// darkness -> dark
+// deeper -> magical dark
+// torch -> normal
+
 function sourceOption(source) {
   return $('<option>')
     .attr('value', source.name)
-    .attr('data-level', source.lightLevel)
     .attr('data-spell-level', source.spellLevel)
     .text(source.name);
 }
@@ -24,9 +39,15 @@ function addSources(sourceList) {
   $('#light-sources').val(sourceList[0].name).change();
 }
 
+function error() {
+  console.log(arguments[1] + " in sources.json")
+}
+
 function darknessReady() {
   $('#light-sources').change(sourceChange);
   if ($('#light-sources option').length === 0) {
-    $.getJSON('data/sources.json').done(addSources);
+    $.getJSON('data/sources.json')
+      .done(addSources)
+      .fail(error);
   }
 }
